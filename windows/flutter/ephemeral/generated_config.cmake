ef1f2d2de39fca4d061fbec0e78adff25623beca
muhammadhaseebb <104450111+muhammadhaseebb@users.noreply.github.com>
2022-06-02 21:14:55 +0500

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Flutter\\AR cart shop" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=D:\\Flutter\\AR cart shop"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Flutter\\AR cart shop\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Flutter\\AR cart shop"
+  "FLUTTER_TARGET=D:\\Flutter\\AR cart shop\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Flutter\\AR cart shop\\.dart_tool\\package_config.json"
+)
